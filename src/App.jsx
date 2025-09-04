3d87f4cd8014ead11009ae3495eb249d20576255
MasadRayan <masadrayan2002@gmail.com>
2025-09-04 16:10:26 +0600

    favicon and Title added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import Hero from './sections/hero';
 import { StarsCanvas } from './components/StarsCanvas/StarsCanvas';
+import ShowcaseSectioon from './sections/ShowcaseSectioon';
 
 
 const App = () => {
   return (
+
+    
     <div className="relative w-full min-h-screen ">
       {/* Background Stars */}
       <StarsCanvas />
 
-      {/* Your Portfolio Content */}
+      {/*Portfolio Content */}
       <Hero />
+      <ShowcaseSectioon />
     </div>
   );
 };
---
+++ src/sections/ShowcaseSectioon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShowcaseSectioon = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ShowcaseSectioon;
