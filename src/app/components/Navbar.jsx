bc634047c228bcfc483adaddf152f2700f8ccaaf
MasadRayan <masadrayan2002@gmail.com>
2025-11-27 15:26:10 +0600

    Navbar blured

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 
 
     return (
-        <div className=" mx-1 md:mx-10 sticky top-0 z-100 mb-30 md:mb-0 bg-black/30  md:backdrop-blur-none backdrop-blur-md rounded-b-lg">
+        <div className=" mx-1 md:px-10 sticky top-0 z-100 mb-30 md:mb-0   bg-transparent backdrop-blur-md rounded-b-lg">
             <nav className='flex justify-between items-center py-5 '>
                 {/* icon */}
                 <div>
