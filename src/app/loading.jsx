2d59017be5700b43100f6bf455ccd0c75f54bdd1
MasadRayan <masadrayan2002@gmail.com>
2025-11-29 12:44:09 +0600

    skeleton loader ui added from shadcn

---
+++ src/app/loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div>
+            <div className='flex justify-between items-center'>
+
+            </div>
+        </div>
+    );
+};
+
+export default loading;
