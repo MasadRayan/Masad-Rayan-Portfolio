bd88410d0da6bd19e56ab472a359e9b3d8ea3501
Masad Rayan <masadrayan2002@gmail.com>
2026-06-02 19:35:54 +0600

    feat: update project details and images in ProjectSection component; upgrade dependencies in package.json

--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -15,6 +15,73 @@ const ProjectSection = () => {
                 </span>
             </h1>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-6 p-6'>
+                <div className="lg:col-span-7 bg-black">
+                    <CardContainer className="inter-var h-full">
+                        <CardBody className=" relative group/card hover:shadow-2xl hover:shadow-emerald-500/10 border-white/20 w-full lg:h-[600px] rounded-xl p-6 border flex flex-col">
+                            <CardItem
+                                translateZ="50"
+                                className="text-xl font-bold text-white"
+                            >
+                                BuyNest – <span className="text-[#FF5005]"> E-Commerce Platform</span>
+                            </CardItem>
+
+                            <CardItem translateZ="100" className="w-full mt-4 overflow-hidden rounded-xl h-60">
+                                <Image
+                                    src={'/projects/buynestFull.png'}
+                                    loading="eager"
+                                    height={1000}
+                                    width={1000}
+                                    alt="thumbnail"
+                                    className="w-full min-h-[400px] object-cover object-top transition-transform duration-3000 ease-linear group-hover/card:-translate-y-[calc(100%-240px)] group-hover/card:shadow-xl"
+
+                                />
+                            </CardItem>
+                            <CardItem
+                                as="p"
+                                translateZ="60"
+                                className="text-neutral-500 text-sm w-full dark:text-neutral-300 mt-5 flex-1"
+                            >
+                                BuyNest is a full-stack MERN e-commerce platform featuring secure authentication, role-based dashboards, SSLCommerz payment integration, downloadable PDF invoices, and an AI-powered chatbot using the Gemini API. Users can browse products, place orders, manage purchases, submit reviews, and track transactions, while admins can manage products, users, and orders through a dedicated dashboard. The platform is fully responsive and built with a focus on security, automation, and modern user experience.
+                            </CardItem>
+                            <CardItem
+                                as="div"
+                                translateZ="60"
+                                className="mt-5"
+                            >
+                                <div className="flex flex-wrap gap-2">
+                                    <span className="bg-blue-500/20 text-blue-400 px-3 py-1 text-xs rounded-full">React</span>
+                                    <span className="bg-blue-500 text-white px-3 py-1 text-xs rounded-full">TailwindCSS</span>
+                                    <span className="bg-orange-500/20 text-orange-400 px-3 py-1 text-xs rounded-full">Firebase Auth</span>
+                                    <span className="bg-green-500/20 text-green-400 px-3 py-1 text-xs rounded-full">MongoDB</span>
+                                    <span className="bg-gray-500/20 text-gray-300 px-3 py-1 text-xs rounded-full">Express.js</span>
+                                    <span className="bg-blue-500/20 text-gray-300 px-3 py-1 text-xs rounded-full">Gemini API</span>
+                                    <span className="bg-gray-700 text-gray-300 px-3 py-1 text-xs rounded-full">+7</span>
+                                </div>
+                            </CardItem>
+                            <div className="flex justify-start gap-2 items-center mt-6">
+                                <CardItem
+                                    translateZ={20}
+                                    as="a"
+                                    href="https://tools-final-projectt.web.app/"
+                                    target="__blank"
+                                    className="flex items-center gap-2 bg-[#ff0033] px-4 py-2 rounded-lg text-sm font-medium cursor-pointer"
+                                >
+                                    <FaExternalLinkAlt /> Live Demo
+                                </CardItem>
+                                <CardItem
+                                    translateZ={20}
+                                    as="a"
+                                    href="https://github.com/MasadRayan/Tools-Final-project-client"
+                                    target="__blank"
+                                    className="flex items-center gap-2 bg-gray-700 hover:bg-gray-600 px-4 py-2 rounded-lg text-sm font-medium"
+                                >
+                                    <FaGithub /> GitHub
+                                </CardItem>
+
+                            </div>
+                        </CardBody>
+                    </CardContainer>
+                </div>
                 <div className="lg:col-span-5 bg-black">
                     <CardContainer className="inter-var h-full">
                         <CardBody className=" relative group/card hover:shadow-2xl hover:shadow-emerald-500/10 border-white/20 w-full lg:h-[600px] rounded-xl p-6 border flex flex-col">
@@ -81,7 +148,7 @@ const ProjectSection = () => {
                         </CardBody>
                     </CardContainer>
                 </div>
-                <div className="lg:col-span-7 bg-black">
+                <div className="lg:col-span-5 bg-black">
                     <CardContainer className="inter-var h-full">
                         <CardBody className=" relative group/card hover:shadow-2xl hover:shadow-emerald-500/10 border-white/20 w-full lg:h-[600px] rounded-xl p-6 border flex flex-col">
                             <CardItem
@@ -222,72 +289,7 @@ const ProjectSection = () => {
                         </CardBody>
                     </CardContainer>
                 </div>
-                <div className="lg:col-span-5 bg-black">
-                    <CardContainer className="inter-var h-full">
-                        <CardBody className=" relative group/card hover:shadow-2xl hover:shadow-emerald-500/10 border-white/20 w-full lg:h-[600px] rounded-xl p-6 border flex flex-col">
-                            <CardItem
-                                translateZ="50"
-                                className="text-xl font-bold text-white"
-                            >
-                                Car Doctor – <span className="text-[#FF5005]"> Car Service Platform</span>
-                            </CardItem>
-
-                            <CardItem translateZ="100" className="w-full mt-4 overflow-hidden rounded-xl h-60">
-                                <Image
-                                    src={'/projects/project4Full.png'}
-                                    loading="eager"
-                                    height={1000}
-                                    width={1000}
-                                    alt="thumbnail"
-                                    className="w-full min-h-[400px] object-cover object-top transition-transform duration-3000 ease-linear group-hover/card:-translate-y-[calc(100%-240px)] group-hover/card:shadow-xl"
-
-                                />
-                            </CardItem>
-                            <CardItem
-                                as="p"
-                                translateZ="60"
-                                className="text-neutral-500 text-sm w-full dark:text-neutral-300 mt-5 flex-1"
-                            >
-                                Car Doctor is an intuitive web app for booking car services online. Users can browse services, schedule appointments, and make secure payments easily. The platform features a smart AI chatbot that answers service and booking queries instantly. Built with React, Next.js, Tailwind CSS, and Express, it demonstrates a modern, responsive, and user-friendly car service management solution. The project also have a personalized chatbot integrated using Google AI Studio API to enhance user experience.
-                            </CardItem>
-                            <CardItem
-                                as="div"
-                                translateZ="60"
-                                className="mt-5"
-                            >
-                                <div className="flex flex-wrap gap-2">
-                                    <span className="bg-blue-500/20 text-blue-400 px-3 py-1 text-xs rounded-full">NextJs</span>
-                                    <span className="bg-orange-500/20 text-orange-400 px-3 py-1 text-xs rounded-full">OAuth</span>
-                                    <span className="bg-blue-500 text-white px-3 py-1 text-xs rounded-full">TailwindCSS</span>
-                                    <span className="bg-green-500/20 text-green-400 px-3 py-1 text-xs rounded-full">MongoDB</span>
-                                    <span className="bg-gray-500/20 text-gray-300 px-3 py-1 text-xs rounded-full">Express.js</span>
-                                    <span className="bg-gray-700 text-gray-300 px-3 py-1 text-xs rounded-full">+4</span>
-                                </div>
-                            </CardItem>
-                            <div className="flex justify-start gap-2 items-center mt-6">
-                                <CardItem
-                                    translateZ={20}
-                                    as="a"
-                                    href="https://car-doctor-nextjs-nu.vercel.app/"
-                                    target="__blank"
-                                    className="flex items-center gap-2 bg-[#ff0033] px-4 py-2 rounded-lg text-sm font-medium cursor-pointer"
-                                >
-                                    <FaExternalLinkAlt /> Live Demo
-                                </CardItem>
-                                <CardItem
-                                    translateZ={20}
-                                    as="a"
-                                    href="https://github.com/MasadRayan/MotorFix"
-                                    target="__blank"
-                                    className="flex items-center gap-2 bg-gray-700 hover:bg-gray-600 px-4 py-2 rounded-lg text-sm font-medium"
-                                >
-                                    <FaGithub /> GitHub
-                                </CardItem>
-
-                            </div>
-                        </CardBody>
-                    </CardContainer>
-                </div>
+                
 
             </div>
         </section>
